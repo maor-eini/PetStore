60da89610de42b9daf9702ef2c60934b32359b2f
Maor Eini <maor.eini@gmail.com>
2016-09-02 05:09:32 +0300

    ME: Initial Project Structure

--- src/PetStore/Startup.cs
+++ src/PetStore/Startup.cs
@@ -1,17 +1,42 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PetStore.Models;
 
 namespace PetStore
 {
     public class Startup
     {
+        public IHostingEnvironment hostingEnvironment { get; private set; }
+        public IConfiguration Configuration { get; private set; }
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
+        private void ConfigureSettings(IServiceCollection services)
+        {
+            var Configuration = new ConfigurationBuilder()
+                .AddJsonFile("PetStoreSettings.json")
+                .Build();
+
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<PetStoreDB>(options => options
+                 .UseSqlServer(Configuration["ConnectionStrings: DefaultConnection"]));
+
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
+
+            ConfigureSettings(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -19,10 +44,30 @@ namespace PetStore
         {
             loggerFactory.AddConsole();
 
+            app.UseWelcomePage();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
             }
+
+            // Enable serving default and static files
+            app.UseFileServer();
+
+            app.UseStatusCodePages();
+
+            // Enable Mvc routes
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("areaRoute", "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
 
             app.Run(async (context) =>
             {
