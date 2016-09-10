ebda346317bbfe84767985099ac05cf598843d4d
Maor Eini <maor.eini@gmail.com>
2016-09-10 13:51:13 +0300

    ME: Fix mvc routing

--- src/PetStore/Controllers/HomeController.cs
+++ src/PetStore/Controllers/HomeController.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PetStore.Controllers
 {
+
     public class HomeController : Controller
     {
         public IActionResult Index()
