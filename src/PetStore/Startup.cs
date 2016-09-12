b88bd4de689edeb9198ba62f341ecc108d997b7b
Sapirle <Sapirle@SAPIRLE-LAP>
2016-09-13 01:25:47 +0300

    SE: Add support for register and login

--- src/PetStore/Startup.cs
+++ src/PetStore/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using PetStore.Data;
+using PetStore.Data.Repositories;
+using PetStore.Data.Repositories.Interfaces;
 using PetStore.Models;
 using PetStore.ViewModels;
 using System.IO;
@@ -52,6 +54,8 @@ namespace PetStore
                     .UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IPetRepository, PetRepository>();
+            services.AddScoped<IUserAddressRepository, UserAddressRepository>();
 
             Mapper.Initialize(config =>
             {
