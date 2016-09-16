ce4b23719903d767c8b3f4b4caca8165b207e791
Sapirle <Sapirle@SAPIRLE-LAP>
2016-09-16 12:32:32 +0300

    -shopping cart css

--- src/PetStore/wwwroot/js/FacebookIntegration.js
+++ src/PetStore/wwwroot/js/FacebookIntegration.js
@@ -1,24 +1,49 @@
 ﻿
 $(document).ready(function () {
-    $("#cart-num-items").hide();
-    var numItems = localStorage['cart-num-items'];
-    if(!numItems)
-        localStorage['cart-num-items'] = 0;
+    var numItems = sessionStorage.getItem('cart-num-items');
+    console.log(numItems);
+    if (!numItems || numItems === 0) {
+        console.log("in if")
+        sessionStorage.setItem('cart-num-items', 0);
+        $("#cart-num-items").hide();
+    }
+    else {
+        console.log("in else")
+        $("#cart-num-items").show();
+    }
+    $("#cart-num-items").html(numItems);
+
 });
 
+var getNumOfItems = function () {
+    return sessionStorage.getItem('cart-num-items') || 0;
+}
 
+$(".minus").click(function(event){
+    event.preventDefault();
+    $.post({
+        url: this.href,
+        success: function (data) {
+            console.log("quntity increased");
+            var prevItems = sessionStorage.getItem('cart-num-items');
+            $("#cart-num-items").html(++prevItems);
+            sessionStorage.setItem('cart-num-items', prevItems)
+            $("#cart-num-items").show();
+        }
+    });
+})
 
 $(".add-to-cart-btn").click(function (event) {
+
     event.preventDefault();
-    var prevItems = parseInt(localStorage['cart-num-items']);
-    $("#cart-num-items").html(++prevItems);
-    $("#cart-num-items").show();
-
-
     $.post({
-        url:this.href,
+        url: this.href,
         success: function (data) {
             console.log("added to cart");
+            var prevItems = sessionStorage.getItem('cart-num-items');
+            $("#cart-num-items").html(++prevItems);
+            sessionStorage.setItem('cart-num-items', prevItems)
+            $("#cart-num-items").show();
         },
         error: function () {
             console.log("failed to add to cart");
